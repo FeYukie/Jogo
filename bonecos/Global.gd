e2929cf3f7dfd908cd326a7909676b83e3b514d1
Fernanda Yukie Furuyama <135060199+FeYukie@users.noreply.github.com>
2026-09-02 21:33:07 -0300

    Add files via upload

--- bonecos/Global.gd
+++ bonecos/Global.gd
@@ -1,2 +1,11 @@
 extends Node
 var personagem_selecionado : SpriteFrames
+
+var vidas: int = 3
+
+func perder_vida(motivo: String = "") -> void:
+	vidas -= 1
+	print("Vida perdida por: ", motivo, ". Vidas restantes: ", vidas)
+	
+	# Se você tiver sinal para atualizar os corações da tela:
+	# emit_signal("vida_alterada", vidas)
