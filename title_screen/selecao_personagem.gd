0991cbdc622e4847312509becca70ac64e6d15f9
Fernanda Yukie Furuyama <135060199+FeYukie@users.noreply.github.com>
2026-09-09 21:24:11 -0300

    Add files via upload

--- title_screen/selecao_personagem.gd
+++ title_screen/selecao_personagem.gd
@@ -9,4 +9,4 @@ func _on_botao_feminino_pressed() -> void:
 	ir_para_o_jogo()
 
 func ir_para_o_jogo():
-	get_tree().change_scene_to_file("res://escola/sala_de_aula.tscn")
+	get_tree().change_scene_to_file("res://patio/terrain/assets/patio.tscn")
